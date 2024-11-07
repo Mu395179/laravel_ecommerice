8e40e4fe77a705dfbc4e625a484b96c37bc4ea61
mu395179 <d128579744@gmail.com>
2024-11-07 15:11:31 +0800

    新增brand model 和頁面

--- resources/views/layouts/test.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-<a href="{{ route('logout') }}"
- onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
- <div class="text">Logout</div>
-</a>
-<form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
-    @csrf
-</form>
