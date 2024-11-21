92b503869370c7ff5d54663de6b607e96e21b1e0
mu395179 <d128579744@gmail.com>
2024-11-21 13:09:55 +0800

    解決js 無法渲染問題 增加品牌篩選功能

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,43 +10,54 @@ class ShopController extends Controller
 {
     public function index(Request $request)
     {
-        $size = $request->query('size') ? $request->query('size'):12;
-        $o_colimn = "";
-        $o_order="";
-        $order = $request->query('order')?$request->query('order'):-1;
-        switch($order)
-        {
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = ""; // 修正拼寫
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+        $f_brands = $request->query('brands');
+
+        switch ($order) {
             case 1:
-                $o_colimn='created_at';
-                $o_order='DESC';
+                $o_column = 'created_at';
+                $o_order = 'DESC';
                 break;
             case 2:
-                $o_colimn='created_at';
-                $o_order='ASC';
+                $o_column = 'created_at';
+                $o_order = 'ASC';
                 break;
             case 3:
-                $o_colimn='sale_price';
-                $o_order='ASC';
+                $o_column = 'sale_price';
+                $o_order = 'ASC';
                 break;
             case 4:
-                $o_colimn='sale_price';
-                $o_order='DESC';
+                $o_column = 'sale_price';
+                $o_order = 'DESC';
                 break;
-            default;
-            $o_colimn='id';
-                $o_order='DESC';
-
+            default:
+                $o_column = 'id';
+                $o_order = 'DESC';
         }
-        $products = Product::orderBy($o_colimn,$o_order)->paginate($size);
-        return view('shop',compact('products','size','order'));
+
+        $brands = Brand::orderBy('name', 'ASC')->get();
+
+        $products = Product::where(function($query) use($f_brands) {
+            if (!empty($f_brands)) {
+                $query->whereIn('brand_id', explode(',', $f_brands));
+            }
+        })
+        ->orderBy($o_column, $o_order)
+        ->paginate($size);
+
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands'));
     }
 
     public function product_details($product_slug)
     {
-        $product = Product::where('slug',$product_slug)->first();
-        $rproducts = Product::where('slug','<>',$product_slug)->get()->take(8);
-        return view('details',compact('product','rproducts'));
+        $product = Product::where('slug', $product_slug)->first();
+        $rproducts = Product::where('slug', '<>', $product_slug)
+            ->limit(8)
+            ->get();
+
+        return view('details', compact('product', 'rproducts'));
     }
-
-
 }
